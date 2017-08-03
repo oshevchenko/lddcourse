da0a5c9792094c9b754fee9926a4378f6924a076
y.hrabo <yaroslav.hrabovskyi@gmail.com>
2017-08-03 22:44:19 +0300

    Minors handling is added. Proc FS reading is added.

--- task2/chardev.c
+++ task2/chardev.c
@@ -1,12 +1,21 @@
 /*
- *  chardev.c: Creates a read-only char device that says how many times
- *  you've read from the dev file
+ *  Creates a char device.
+ *  Device reads input and try convert it
+ *  to number with base 10.
+ *
+ *  On request to device file it returns last entered value.         
+ *  On request to proc file it returns all last entered values
+ *  on all device parts.
+ *
+ *  Issue: if request followed before write, incorrect
+ *  pointer is get and error messages are raised.
  */
 
 #include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/types.h>
 #include <asm/uaccess.h>	/* for put_user */
 
 /*  
@@ -18,12 +27,15 @@ static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+static ssize_t proc_fs_read(struct file *, char *, size_t, loff_t *);
 
-#define BUF_LEN 80				/* Max length of the message from the device */
+#define BUF_LEN 40				/* Max length of the message from the device */
 #define DEVICE_NAME "chardev"	/* Dev name as it appears in /proc/devices   */
 #define MAX_MINOR 4             /* Maximum files in /dev/                    */
+#define NO 0
+#define YES 1
 #define NUM_BASE_10 10          /* Number base to use during convert str to l*/
-#define PROC_FS_NAME "coolchardev"/* Name for /proc/                         */
+#define PROC_FS_NAME "chardev"	/* Name for /proc/                           */
 #define SUCCESS 0
 #define UNSUCCESS -1
 
@@ -34,8 +46,9 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 static int Major;					/* Major number assigned to our device driver*/
 static int Device_Open = 0;			/* Is device open?  
 				 				     * Used to prevent multiple access to device */
-static char msg[MAX_MINOR][BUF_LEN];/* The msg the device will give when asked   */
-static char *msg_Ptr[MAX_MINOR];
+static char msg[MAX_MINOR][BUF_LEN] = {};/* The msg the device will give when asked   */
+static char *msg_Ptr[MAX_MINOR];    // temp ptr for device read/write functions
+static char *msg_ptr_proc_fs;       // for proc fs functions
 
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
@@ -47,7 +60,7 @@ static struct file_operations fops = {
 
 static struct file_operations proc_fs_fops = {
 	.owner = THIS_MODULE,
-	.read = device_read
+	.read = proc_fs_read
 };
 
 struct proc_dir_entry *proc_file_entry;
@@ -150,24 +163,27 @@ static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
 			   loff_t * offset)
 {
 	int bytes_read = 0;         // Num of bytes actually written to the buf 
-	int *minor = 0;                 /* contains minor value      */
+	int minor = 0;              /* contains minor value      */
 
-	minor = filp->private_data;     // get minor
+	minor = *((int *)filp->private_data);     // get minor
+	if (minor >= MAX_MINOR) {
+		return 0;
+	}
 
 	/*
 	 * If we're at the end of the message, 
 	 * return 0 signifying end of file 
 	 */
-	if (*msg_Ptr[*minor] == 0) {
+	if (*msg_Ptr[minor] == 0) {
 
-		msg_Ptr[*minor] = msg[*minor];          /* Return ptr po start pos   */
+		msg_Ptr[minor] = msg[minor];          /* Return ptr po start pos   */
 		return 0;
 	}
 
 	/* 
 	 * Actually put the data into the buffer 
 	 */
-	while (length && *msg_Ptr[*minor]) {
+	while (length && *msg_Ptr[minor]) {
 
 		/* 
 		 * The buffer is in the user data segment, not the kernel 
@@ -175,7 +191,7 @@ static ssize_t device_read(struct file *filp,	/* see include/linux/fs.h   */
 		 * put_user which copies data from the kernel data segment to
 		 * the user data segment. 
 		 */
-		put_user(*(msg_Ptr[*minor]++), buffer++);
+		put_user(*(msg_Ptr[minor]++), buffer++);
 
 		length--;
 		bytes_read++;
@@ -194,34 +210,71 @@ static ssize_t
 device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
 
-	int *i = 0;                 /* contains minor value      */
+	int minor = 0;              /* contains minor value      */
 	int bytes_written = 0;		/* bytes write to the buffer */
 	int convert_result = 0;     /* result of convertion      */
-	long converted_num = 0;      // result of convertion from str
+	long converted_num = 0;     // result of convertion from str
 
-	i = filp->private_data;     // get minor
-	msg_Ptr[*i] = msg[*i];      // get ptr to minor related data
+	minor = *((int *)filp->private_data);     // get minor
+	msg_Ptr[minor] = msg[minor];      // get ptr to minor related data
 
 
 	if (len > BUF_LEN) {
 		return UNSUCCESS;
 	}
 
-	copy_from_user(msg_Ptr[*i], buff, len);
+	copy_from_user(msg_Ptr[minor], buff, len);
 
-	convert_result = kstrtol(msg[*i], NUM_BASE_10, &converted_num);
+	convert_result = kstrtol(msg[minor], NUM_BASE_10, &converted_num);
 
-	if (convert_result == -ERANGE) {
-		printk(KERN_INFO "Entered number is too big or too small.\n");
-		bytes_written = UNSUCCESS;
-	}
-	else if (convert_result == -EINVAL) {
+	if (convert_result) {
 		printk(KERN_INFO "Couldn't convert your number. Try again.\n");
 		bytes_written = UNSUCCESS;
 	}
 	else {
 		bytes_written = len;
 	}
+	*(msg_Ptr[minor] + len) = 0;
 
 	return bytes_written;
+}
+
+/* 
+ * Called when /proc/ file is read.
+ */
+static ssize_t proc_fs_read(struct file *filp,	/* see include/linux/fs.h   */
+			   char *buffer,					/* buffer to fill with data */
+			   size_t length,					/* length of the buffer     */
+			   loff_t * offset)
+{
+	static u_int32_t finished = NO;  			// indicates if data read finished
+	u_int8_t bytes_read = 0;
+	u_int8_t i = 0;             				// array index
+	
+	if (finished == YES) {
+		finished = NO;
+		return 0;
+	}
+
+	// In this implementation just exit if user buffer too small
+	if (length < (BUF_LEN * MAX_MINOR)) {
+		return 0;
+	}
+
+	finished = YES;
+	// copy all minors data
+	for (i = 0; i < MAX_MINOR; i++) {
+		msg_ptr_proc_fs = msg[i];
+
+		do {
+			put_user(*msg_ptr_proc_fs++, buffer++);
+			bytes_read++;
+		}
+		while (*msg_ptr_proc_fs);
+	}
+
+	/* 
+	 * Most read functions return the number of bytes put into the buffer
+	 */
+	return bytes_read;
 }
