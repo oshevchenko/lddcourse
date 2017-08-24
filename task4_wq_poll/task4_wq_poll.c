51adb649c0974dc5f12ea31f2b9c620cee577aff
yhrabo <yaroslav.hrabovskyi@gmail.com>
2017-08-24 22:07:14 +0300

    Working version of module to find min, max and avg values from input user data.

--- task4_wq_poll/task4_wq_poll.c
+++ task4_wq_poll/task4_wq_poll.c
@@ -1,16 +1,6 @@
-/*
- * After write bytes to the driver, 3 processes are started
- * (find min, max and mean value among written bytes).
- * 
- * Issues at the moment:
- * 	- read function doesnt act as typical block/nonblock IO
- * 	- no guarantees that result (min, max and mean) will be
- *	  returned to the same user, which wrote data.
- */
-
 #include "task4_wq_poll.h"
 
-static unsigned int major;				/* Major number						*/
+static unsigned int major;				// Major number						*/
 
 static struct file_operations task4_dev_fops = {
 	.owner = THIS_MODULE,
@@ -21,161 +11,254 @@ static struct file_operations task4_dev_fops = {
 	.write = task4_dev_write,
 };
 
-static struct task4_dev task4_device;
+static struct task4_dev *task4_device;
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yaroslav Hrabovskyi <yaroslav.hrabovskyi@gmail.com>");
 
 /*
- * Open /dev/ file
+ * Open /dev/ file.
  */
 int task4_dev_open(struct inode *inode, struct file *filp)
 {
-	filp->private_data = container_of(inode->i_cdev, struct task4_dev, dev);
+	struct task4_dev *dev;
 
+	dev = container_of(inode->i_cdev, struct task4_dev, dev);
+	filp->private_data = dev;
+
+	if (mutex_lock_interruptible(&dev->mutex)) {
+		return -ERESTARTSYS;
+	}
+
+	// Allocate memory for data
+	if (dev->data == NULL) {
+		dev->data = kmalloc(MAX_BUF_SIZE, GFP_KERNEL);
+		if (dev->data == NULL) {
+			mutex_unlock(&dev->mutex);
+			return -ENOMEM;
+		}
+	}
+	dev->num_open_file++;
+
+	mutex_unlock(&dev->mutex);
+	printk(KERN_INFO "%s (%i) opened /dev/ file.\n",
+			current->comm, current->pid);
 	try_module_get(THIS_MODULE);
 	return 0;
 }
 
 /*
- * Close /dev/ file
+ * Close /dev/ file.
  */
 int task4_dev_release(struct inode *inode, struct file *filp)
 {
+	struct task4_dev *dev;
+
+	dev = container_of(inode->i_cdev, struct task4_dev, dev);
+	if (mutex_lock_interruptible(&dev->mutex)) {
+		return -ERESTARTSYS;
+	}
+
+	if (!(--dev->num_open_file)) {
+		kfree(dev->data);
+		dev->bytes_written = 0;
+		dev->data = NULL;
+	}
+
+	mutex_unlock(&dev->mutex);
+	printk(KERN_INFO "%s (%i) closed /dev/ file.\n",
+			current->comm, current->pid);
 	module_put(THIS_MODULE);
 	return 0;
 }
 
 /*
- * Write to /dev/ file
+ * Write to /dev/ file.
  */
-ssize_t task4_dev_write(struct file *filp, const char __user *buf, size_t len, loff_t *offs)
+ssize_t task4_dev_write(struct file *filp, const char __user *buf,
+						size_t len, loff_t *offs)
 {
-	int i;
-	struct task4_dev *dev = filp->private_data;
+	struct task4_dev *dev;
 
-	down_write(&dev->rw_sem);
-	if (len > MAX_BUF_SIZE) {
-		printk(KERN_INFO "Couldn't copy user data: %d bytes.\n", (int)len);
-		return -EFAULT;
+	dev = filp->private_data;
+
+	if (mutex_lock_interruptible(&dev->mutex)) {
+		return -ERESTARTSYS;
 	}
 
-	/* Previous data isn't handled yet */
-	while (dev->bytes_written > 0) {
-		up_write(&dev->rw_sem);
-
-		/* Wait while written data will be handled */
-		if (wait_event_interruptible(dev->wrt_q, (dev->bytes_written == 0))) {
+	while (dev->bytes_written) {
+		//Previous data is being processing
+		mutex_unlock(&dev->mutex);
+		if (filp->f_flags & O_NONBLOCK) {
+			return -EAGAIN;
+		}
+		if (wait_event_interruptible(dev->wrt_q,
+									dev->bytes_written == 0)) {
 			return -ERESTARTSYS;
 		}
-		down_write(&dev->rw_sem);
+		if (mutex_lock_interruptible(&dev->mutex)) {
+			return -ERESTARTSYS;
+		}
 	}
-
+	// Buffer is ready to take data
 	copy_from_user(dev->data, buf, len);
 	dev->bytes_written = len;
-	up_write(&dev->rw_sem);
+	atomic_set(&dev->opers_left, 3);
+	mutex_unlock(&dev->mutex);
 
-	for (i = 0; i < MAX_NUM_WQ; i++) {
-		schedule_work(&dev->calc_data_wq[i]);
-	}
+	schedule_work(&dev->find_min_wq);
+	schedule_work(&dev->find_max_wq);
+	schedule_work(&dev->find_avg_wq);
+
+	printk(KERN_INFO "%s (%i) wrote %lu bytes.\n",
+			current->comm, current->pid, len);
 
 	return len;
 }
 /*
- * Read from /dev/ file
+ * Read from /dev/ file.
  */
-ssize_t task4_dev_read(struct file *filp, char __user *buf, size_t len, loff_t *offs)
+ssize_t task4_dev_read(struct file *filp, char __user *buf,
+						size_t len, loff_t *offs)
 {
-	struct task4_dev *dev = filp->private_data;
-	down_read(&dev->rw_sem);
-	if (mutex_lock_interruptible(&dev->result_data.mutex)) {
+	char buf_from[3];
+	int read_bytes;
+	struct task4_dev *dev;
+
+	dev = filp->private_data;
+	if (mutex_lock_interruptible(&dev->mutex)) {
 		return -ERESTARTSYS;
 	}
 
-	buf[MIN_POS] = dev->result_data.min;
-	buf[MAX_POS] = dev->result_data.max;
-	buf[MEAN_POS] = dev->result_data.mean;
+	while (atomic_read(&dev->opers_left) || (dev->bytes_written == 0)) {
+		// Data is not ready or it is absent
+		mutex_unlock(&dev->mutex);
+		if (filp->f_flags & O_NONBLOCK) {
+			return -EAGAIN;
+		}
 
-	if (dev->bytes_written > 0) {
-		/* Data was read. */
+		if (wait_event_interruptible(dev->rd_q,
+				(atomic_read(&dev->opers_left) || (dev->bytes_written != 0)))) {
+			return -ERESTARTSYS;
+		}
+
+		if (mutex_lock_interruptible(&dev->mutex)) {
+			return -ERESTARTSYS;
+		}
+	}
+
+	// Data is ready
+	if (len >= 3) {
+		buf_from[0] = dev->min_value;
+		buf_from[1] = dev->max_value;
+		buf_from[2] = dev->avg_value;
+		copy_to_user(buf, buf_from, 3);
 		dev->bytes_written = 0;
-		/* Clear state that data is ready */
-		dev->result_data.stateBmp = 0;
-		len = 3;
+		read_bytes = 3;
 	}
 	else {
-		len = 0;
+		read_bytes = 0;
 	}
 
-	mutex_unlock(&dev->result_data.mutex);
-	up_read(&dev->rw_sem);
+	mutex_unlock(&dev->mutex);
+	// Wake up writers
 	wake_up_interruptible(&dev->wrt_q);
 
-	return len;
+	printk(KERN_INFO "%s (%i) read %d bytes.\n",
+			current->comm, current->pid, read_bytes);
+	return read_bytes;
 }
 
 /*
- * Poll /dev/ file
+ * Poll /dev/ file.
  */
-unsigned int task4_dev_poll(struct file *filp, struct poll_table_struct *poll_table)
+unsigned int task4_dev_poll(struct file *filp,
+							struct poll_table_struct *poll_table)
 {
+	struct task4_dev *dev;
 	unsigned int mask = 0;
 
-	struct task4_dev *dev = filp->private_data;
-	if (mutex_lock_interruptible(&dev->result_data.mutex)) {
-		return -ERESTARTSYS;
+	dev = filp->private_data;
+	if (mutex_lock_interruptible(&dev->mutex)) {
+		return mask;
 	}
 
-	if ((dev->result_data.stateBmp & TASK_COMPL) == TASK_COMPL) {
-		mask = POLLIN | POLLRDNORM;
+	poll_wait(filp, &dev->rd_q, poll_table);
+	poll_wait(filp, &dev->wrt_q, poll_table);
+
+	if (!atomic_read(&dev->opers_left) && !dev->bytes_written) {
+		mask |= POLLOUT | POLLWRNORM;
 	}
 
-	mutex_unlock(&dev->result_data.mutex);
-	printk(KERN_INFO "Poll mask %d called.\n", mask);
+	if (!atomic_read(&dev->opers_left) && dev->bytes_written) {
+		mask |= POLLIN | POLLRDNORM;
+	}
 
+	mutex_unlock(&dev->mutex);
 	return mask;
 }
 
+/*
+ * Called whe insmod is executed.
+ */
 int task4_init_module(void)
 {
-	dev_t dev, result;
-	int i;
+	dev_t dev;							// Contains major
+	int result;
 
-	result = alloc_chrdev_region(&dev, TASK4_DEV_MINOR, MAX_NUM_DEVICES, DEVICE_NAME);
+	// Get major
+	result = alloc_chrdev_region(&dev, TASK4_DEV_MINOR,
+								MAX_NUM_DEVICES, DEVICE_NAME);
 
 	if (result < 0) {
-		printk(KERN_INFO "Device was not registered: %d.\n", result);
+		printk(KERN_WARNING "Device major was not registered: %d.\n", result);
 		return result;
 	}
-
 	major = MAJOR(dev);
-	printk(KERN_INFO "Registered major: %d\n", major);
+	printk(KERN_NOTICE "Registered major: %d.\n", major);
 
-	memset(&task4_device, 0, sizeof(struct task4_dev));
-
-	/* Setup functions to use for workqueue */
-	task4_device.find_value_fn[MIN_POS] = find_min_value;
-	task4_device.find_value_fn[MAX_POS] = find_max_value;
-	task4_device.find_value_fn[MEAN_POS] = find_mean_value;
-	for (i = 0; i < MAX_NUM_WQ; i++) {
-		INIT_WORK(&task4_device.calc_data_wq[i], task4_device.find_value_fn[i]);
+	// Allocate memory for device structure
+	task4_device = kmalloc(sizeof(struct task4_dev), GFP_KERNEL);
+	if (task4_device == NULL) {
+		printk(KERN_WARNING "Allocation memory for device structure failed.\n");
+		unregister_chrdev_region(MKDEV(major, 0), MAX_NUM_DEVICES);
+		return -ENOMEM;
 	}
 
-	/* Initialize a device */
-	result = task4_setup_dev(&task4_device);
+	// Clear device structure data
+	memset(task4_device, 0, sizeof(struct task4_dev));
+
+	// Initialize device
+	result = task4_setup_dev(task4_device);
 	if (result < 0) {
-		printk(KERN_ALERT "Device was not added.\n");
+		printk(KERN_WARNING "Device was not added.\n");
+		unregister_chrdev_region(MKDEV(major, 0), MAX_NUM_DEVICES);
+		kfree(task4_device);
+		return 0;
 	}
 
-	mutex_init(&task4_device.result_data.mutex);
-	init_rwsem(&task4_device.rw_sem);
+	// Initislize work queues
+	INIT_WORK(&task4_device->find_avg_wq, &find_avg_value);
+	INIT_WORK(&task4_device->find_max_wq, &find_max_value);
+	INIT_WORK(&task4_device->find_min_wq, &find_min_value);
 
-	init_waitqueue_head(&(task4_device.wrt_q));
+	mutex_init(&task4_device->mutex);
+	init_waitqueue_head(&task4_device->wrt_q);
+	init_waitqueue_head(&task4_device->rd_q);
 
 	return result;
 }
 
+/*
+ * Called wehn rmmod is module.
+ */
 void task4_cleanup_module(void)
 {
+	cdev_del(&task4_device->dev);
+	kfree(task4_device);
+	task4_device = NULL;
 	unregister_chrdev_region(MKDEV(major, 0), MAX_NUM_DEVICES);
-	cdev_del(&task4_device.dev);
+	printk(KERN_INFO "Device was unregistered.\n");
 }
 
 static int task4_setup_dev(struct task4_dev *device)
@@ -193,30 +276,35 @@ static int task4_setup_dev(struct task4_dev *device)
 void find_min_value(struct work_struct *wq)
 {
 	int i;
-	char min_value;
+	unsigned char min;
 	unsigned char *buf;
 
-	struct task4_dev *dev = container_of(wq, struct task4_dev, calc_data_wq[0]);
+	struct task4_dev *dev = container_of(wq, struct task4_dev, find_min_wq);
 
-	down_read(&dev->rw_sem);
-	buf = dev->data;
-	min_value = buf[0];
-	for (i = 1; i < dev->bytes_written; i++) {
-		if (min_value > buf[i]) {
-			min_value = buf[i];
-		}
-	}
-
-	if (mutex_lock_interruptible(&dev->result_data.mutex)) {
+	// Lock access to structure
+	if (mutex_lock_interruptible(&dev->mutex)) {
 		return;
 	}
-	dev->result_data.min = min_value;
-	/* Operation is completed */
-	dev->result_data.stateBmp |= (1U << MIN_POS);
-	mutex_unlock(&dev->result_data.mutex);
 
-	up_read(&dev->rw_sem);
+	buf = dev->data;
+	min = 255;
+	// Find min value
+	for (i = 0; i < dev->bytes_written; i++) {
+		if (min > buf[i]) {
+			min = buf[i];
+		}
+	}
+	dev->min_value = min;
 
+	mutex_unlock(&dev->mutex);
+
+	// If all tasks from work queues are completed
+	if (atomic_dec_and_test(&dev->opers_left)) {
+		wake_up_interruptible(&dev->rd_q);
+	}
+
+	printk(KERN_INFO "%s is called. Bytes processed %u. Value %u found.\n",
+					__func__, dev->bytes_written, min);
 	return;
 }
 
@@ -226,64 +314,77 @@ void find_min_value(struct work_struct *wq)
 void find_max_value(struct work_struct *wq)
 {
 	int i;
-	char max_value;
+	unsigned char max;
 	unsigned char *buf;
 
-	struct task4_dev *dev = container_of(wq, struct task4_dev, calc_data_wq[1]);
+	struct task4_dev *dev = container_of(wq, struct task4_dev, find_max_wq);
 
-	down_read(&dev->rw_sem);
-	buf = dev->data;
-	max_value = buf[0];
-	for (i = 1; i < dev->bytes_written; i++) {
-		if (max_value < buf[i]) {
-			max_value = buf[i];
-		}
-	}
-	if (mutex_lock_interruptible(&dev->result_data.mutex)) {
+	// Lock access to structure
+	if (mutex_lock_interruptible(&dev->mutex)) {
 		return;
 	}
-	dev->result_data.max = max_value;
-	/* Operation is completed */
-	dev->result_data.stateBmp |= (1U << MAX_POS);
-	mutex_unlock(&dev->result_data.mutex);
 
-	up_read(&dev->rw_sem);
+	//mdelay(2);
+	buf = dev->data;
+	max = 0;
+	// Find max value
+	for (i = 0; i < dev->bytes_written; i++) {
+		if (max < buf[i]) {
+			max = buf[i];
+		}
+	}
+	dev->max_value = max;
 
+	mutex_unlock(&dev->mutex);
+
+	// If all tasks from work queues are completed
+	if (atomic_dec_and_test(&dev->opers_left)) {
+		wake_up_interruptible(&dev->rd_q);
+	}
+
+	printk(KERN_INFO "%s is called. Bytes processed %u. Value %u found.\n",
+					__func__, dev->bytes_written, max);
 	return;
 }
 
 /*
- * Find mean value from values in buffer.
+ * Find avg value from values in buffer.
  */
-void find_mean_value(struct work_struct *wq)
+void find_avg_value(struct work_struct *wq)
 {
-	int i, count = 0;
-	long mean_value = 0;
+	unsigned int i, count, sum;
+	unsigned char avg;
 	unsigned char *buf;
 
-	struct task4_dev *dev = container_of(wq, struct task4_dev, calc_data_wq[2]);
+	struct task4_dev *dev = container_of(wq, struct task4_dev, find_avg_wq);
 
-	down_read(&dev->rw_sem);
-	buf = dev->data;
-
-	for (i = 0; i < dev->bytes_written; i++) {
-		mean_value += buf[i];
-		count++;
-	}
-
-	if (mutex_lock_interruptible(&dev->result_data.mutex)) {
+	// Lock access to structure
+	if (mutex_lock_interruptible(&dev->mutex)) {
 		return;
 	}
 
-	if (count > 0) {
-		dev->result_data.mean = (unsigned char)(mean_value / count);
+	buf = dev->data;
+	sum = 0, count = 0, avg = 0;
+
+	// Find min value
+	if (dev->bytes_written > 0) {
+		for (i = 0; i < dev->bytes_written; i++) {
+			sum += buf[i];
+			count++;
+		}
+		avg = sum / count;
 	}
-	/* Operation is completed */
-	dev->result_data.stateBmp |= (1U << MEAN_POS);
-	mutex_unlock(&dev->result_data.mutex);
+	dev->avg_value = avg;
+	
+	mutex_unlock(&dev->mutex);
 
-	up_read(&dev->rw_sem);
+	// If all tasks from work queues are completed
+	if (atomic_dec_and_test(&dev->opers_left)) {
+		wake_up_interruptible(&dev->rd_q);
+	}
 
+	printk(KERN_INFO "%s is called. Bytes processed %u. Value %u found.\n",
+					__func__, dev->bytes_written, avg);
 	return;
 }
 
--- task4_wq_poll/task4_wq_poll.h
+++ task4_wq_poll/task4_wq_poll.h
@@ -1,43 +1,39 @@
-#include <linux/cdev.h>					/* Structure cdev 					*/
-#include <linux/fs.h>					/* file_operations structure 		*/
-#include <linux/kdev_t.h>				/* For MAJOR/MINOR macros			*/
-#include <linux/kernel.h>				/* printfk 							*/
-#include <linux/module.h>				/* For module 						*/
-#include <linux/mutex.h>				/* mutual exclusion 				*/
-#include <linux/poll.h>					/* poll 							*/
-#include <linux/rwsem.h>				/* For semaphore					*/
-#include <asm/uaccess.h>				/* copy from/to user space			*/
+#include <linux/cdev.h>					// Structure cdev
+#include <linux/delay.h>
+#include <linux/fs.h>					// file_operations structure
+#include <linux/kdev_t.h>				// For MAJOR/MINOR macros
+#include <linux/kernel.h>				// printfk
+#include <linux/module.h>				// For module
+#include <linux/mutex.h>				// mutual exclusion
+#include <linux/poll.h>					// poll
+#include <linux/slab.h>					// kmalloc
+#include <asm/atomic.h>					// Atomic operations
+#include <asm/uaccess.h>				// copy from/to user space
 
-#define DEVICE_NAME "chw"				/* Device name 						*/
-#define MAX_BUF_SIZE 10*1024			/* Buffer size    					*/
-#define MAX_NUM_DEVICES 1				/* Number of allowed devices 		*/
-#define MAX_NUM_WQ 3
-#define TASK4_DEV_MINOR 0				/* Minor 							*/
-
-#define MIN_POS  	 0					/* Min position bit or index in arr */
-#define MAX_POS  	 1					/* Max position bit or index in arr */
-#define MEAN_POS   	 2					/* Mean position bit or idx in arr  */
-#define TASK_COMPL 	 0x0007 			/* Task find min, max, mean complete*/
-
-struct min_max_mean {
-	unsigned char min;
-	unsigned char max;
-	unsigned char mean;
-	unsigned int stateBmp;				/* State bit map 					*/
-	struct mutex mutex;
-};
+#define DEVICE_NAME "chw"				// Device name
+#define MAX_BUF_SIZE 10*1024			// Buffer size 
+#define MAX_NUM_DEVICES 1				// Number of allowed devices
+#define TASK4_DEV_MINOR 0				// Minor
 
 struct task4_dev {
-	unsigned int bytes_written;			/* Written bytes to data array		*/
-	unsigned char data[MAX_BUF_SIZE];	/* Contains data to process 		*/
-	struct min_max_mean result_data;	/* Contains min, max and mean values*/
+	unsigned int bytes_written;			// Written bytes to data array
+	unsigned char *data;				// Data to process
 
-	struct cdev dev;					/* Device structure					*/
-	struct rw_semaphore rw_sem;			/* Read/write semaphore 			*/
-	wait_queue_head_t wrt_q;			/* Write queue 						*/
+	unsigned char avg_value;			// Average value found
+	unsigned char min_value;			// Min value found
+	unsigned char max_value;			// Max value found
 
-	struct work_struct calc_data_wq[MAX_NUM_WQ];	/* 						*/
-	void (*find_value_fn[MAX_NUM_WQ])(struct work_struct *);
+	unsigned int num_open_file;			// Number of opened files
+	struct cdev dev;					// Device structure
+	struct mutex mutex;					// mutual exclusive lock 
+	
+	atomic_t opers_left;				// Num of operations left to be executed
+	wait_queue_head_t rd_q;				// Read queue
+	wait_queue_head_t wrt_q;			// Write queue 
+
+	struct work_struct find_avg_wq;		// wq for find avg work
+	struct work_struct find_max_wq;		// wq for find max work
+	struct work_struct find_min_wq;		// wq for find min work
 };
 
 ssize_t task4_dev_read(struct file *, char __user *, size_t, loff_t *);
@@ -48,4 +44,4 @@ int task4_dev_release(struct inode *, struct file *);
 static int task4_setup_dev(struct task4_dev *);
 void find_min_value(struct work_struct *wq);
 void find_max_value(struct work_struct *wq);
-void find_mean_value(struct work_struct *wq);
+void find_avg_value(struct work_struct *wq);
